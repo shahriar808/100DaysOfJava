8dcf9fe3ebd911a38317ca875b941effcf201a3a
shahriar <shasan5525@gmail.com>
2023-06-11 21:09:51 +0600

    Day 25

---
+++ ArrayAndString/JumpGame2.java
@@ -0,0 +1,21 @@
+package Java100.ArrayAndString;
+
+public class JumpGame2 {
+    public static int canJump2(int [] nums){
+            int jumps = 0;
+            int current = 0;
+            int maxReach = 0;
+            for(int i=0;i<nums.length-1;i++){
+                maxReach = Math.max(maxReach,i+nums[i]);
+                if(i==current){
+                    current = maxReach;
+                    jumps++;
+                }
+            }
+            return jumps;
+    }
+    public static void main(String[] args) {
+        int [] nums = {2,3,0,1,4};
+        System.out.println(canJump2(nums));
+    }
+}
