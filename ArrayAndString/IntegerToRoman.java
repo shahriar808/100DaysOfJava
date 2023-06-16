c2fd826fc106dc32a97c5a894d75ad6224e2f407
shahriar <shasan5525@gmail.com>
2023-06-16 23:20:42 +0600

    Day 30

---
+++ ArrayAndString/IntegerToRoman.java
@@ -0,0 +1,16 @@
+package Java100.ArrayAndString;
+
+public class IntegerToRoman {
+        public static String intToRoman(int num) {
+            String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+            String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+            String[] hrns = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+            String[] ths = {"", "M", "MM", "MMM"};
+
+            return ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+        }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(1547));
+    }
+}
